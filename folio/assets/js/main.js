5ca056bddd71a8ed3e375a79ad805480deb66b32
brookepage <30714494+brookepage@users.noreply.github.com>
2017-10-25 14:38:02 +1100

    Time project

---
+++ folio/assets/js/main.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+
+
+// Add jQuery here
+
+// Cycle plugin
+    $('.slides').cycle({
+        fx:     'none',
+        speed:   1,
+        timeout: 70
+    }).cycle("pause");
+
+    // Pause & play on hover
+    $('.slideshow-block').hover(function(){
+        $(this).find('.slides').addClass('active').cycle('resume');
+    }, function(){
+        $(this).find('.slides').removeClass('active').cycle('pause');
+    });
+
+
+
+  });
