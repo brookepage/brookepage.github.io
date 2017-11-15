b1e2b84976dc6ffd109a1f8320112198101294f6
brookepage <30714494+brookepage@users.noreply.github.com>
2017-11-16 08:51:47 +1100

    move buttons

--- studio/assets/js/main.js
+++ studio/assets/js/main.js
@@ -4,6 +4,7 @@ $(document).ready(function() {
 // Add jQuery here
 
 
+//    ----------- shower -----------   //
 
         var showerSound = document.createElement('audio');
         showerSound.setAttribute('src', 'http://www.soundjay.com/misc/sounds/bell-ringing-01.mp3');
@@ -12,11 +13,8 @@ $(document).ready(function() {
           showerSound.play();
         }, true);
     
-    
 
     var playing = false;
-
-
 
 
         $('#shower').click(function() {
@@ -33,20 +31,118 @@ $(document).ready(function() {
             }    
         });
     
-//        $('#shower').click(function() {
-//                if (playing == false) {
-//                showerSound.play();
-//                playing = true;
-//
-//                } else {
-//                    playing = false;
-//                     showerSound.pause();
-//                }    
-//            });
+
+//    ----------- tea -----------   //
+    
+        var teaSound = document.createElement('audio');
+        teaSound.setAttribute('src', 'http://www.soundjay.com/misc/sounds/bell-ringing-01.mp3');
+        $.get();
+        teaSound.addEventListener("load", function() {
+          teaSound.play();
+        }, true);
+    
+
+    var playing = false;
 
 
+        $('#tea').click(function() {
+            
+            if (playing == false) {
+            $(this).addClass('play');    
+            teaSound.play();
+            playing = true;
+            
+            } else {
+                $(this).removeClass('play');
+                playing = false;
+                 teaSound.pause();
+            }    
+        });
+    
+//    ----------- book -----------   //
+    
+        var bookSound = document.createElement('audio');
+        bookSound.setAttribute('src', 'http://www.soundjay.com/misc/sounds/bell-ringing-01.mp3');
+        $.get();
+        bookSound.addEventListener("load", function() {
+          bookSound.play();
+        }, true);
+    
+
+    var playing = false;
 
 
+        $('#book').click(function() {
+            
+            if (playing == false) {
+            $(this).addClass('play');    
+            bookSound.play();
+            playing = true;
+            
+            } else {
+                $(this).removeClass('play');
+                playing = false;
+                 bookSound.pause();
+            }    
+        });
+    
+    
+//    ----------- cooking -----------   //
+    
+        var cookingSound = document.createElement('audio');
+        cookingSound.setAttribute('src', 'http://www.soundjay.com/misc/sounds/bell-ringing-01.mp3');
+        $.get();
+        cookingSound.addEventListener("load", function() {
+          cookingSound.play();
+        }, true);
+    
+
+    var playing = false;
+
+
+        $('#cooking').click(function() {
+            
+            if (playing == false) {
+            $(this).addClass('play');    
+            cookingSound.play();
+            playing = true;
+            
+            } else {
+                $(this).removeClass('play');
+                playing = false;
+                 cookingSound.pause();
+            }    
+        });
+    
+    
+    
+//    ----------- sleeping -----------   //
+    
+        var sleepingSound = document.createElement('audio');
+        sleepingSound.setAttribute('src', 'http://www.soundjay.com/misc/sounds/bell-ringing-01.mp3');
+        $.get();
+        sleepingSound.addEventListener("load", function() {
+          sleepingSound.play();
+        }, true);
+    
+
+    var playing = false;
+
+
+        $('#sleeping').click(function() {
+            
+            if (playing == false) {
+            $(this).addClass('play');    
+            sleepingSound.play();
+            playing = true;
+            
+            } else {
+                $(this).removeClass('play');
+                playing = false;
+                 sleepingSound.pause();
+            }    
+        });
+    
 
 
 
